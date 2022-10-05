3d890ad697f7266ffb51ba8ebfda058552f7d9fc
TheEvilBird <62757686+TheEvilBird@users.noreply.github.com>
2022-10-05 13:14:06 +0300

    New fft

--- src/math/fft_fedshat.cpp
+++ src/math/fft_fedshat.cpp
@@ -1,80 +1,126 @@
-using ll = long long;
+constexpr int P = 998244353;
+using i64 = long long;
 
-constexpr int mod = 998244353, root = 3;
-
-int binpow(int a, int n, int mod) {
-    if (n == 0) {
-        return 1;
+// assume -P <= x < 2P
+int norm(int x) {
+    if (x < 0) {
+        x += P;
     }
-    if (n % 2 == 0) {
-        int b = binpow(a, n / 2, mod);
-        return (b * 1ll * b) % mod;
+    if (x >= P) {
+        x -= P;
     }
-    return (a * 1ll * binpow(a, n - 1, mod)) % mod;
+    return x;
 }
 
-vector<int> W1, W2;
-
-vector<int> ntt(vector<int> a, bool inv = false) {
-    if (a.size() == 1) {
-        return a;
-    }
-    int n = a.size();
-    vector<int> a0, a1;
-    for (int i = 0; i < n; ++i) {
-        if (i % 2 == 0) {
-            a0.push_back(a[i]);
-        } else {
-            a1.push_back(a[i]);
+template<class T>
+T power(T a, int b) {
+    T res = 1;
+    for (; b; b /= 2, a *= a) {
+        if (b % 2) {
+            res *= a;
         }
-    }
-    a0 = ntt(a0, inv);
-    a1 = ntt(a1, inv);
-    int w = 1, wn = W1[n];
-    if (inv) {
-        wn = W2[n];
-    }
-    vector<int> res(n);
-    for (int k = 0; k < n / 2; ++k) {
-        int t = (w * 1ll * a1[k]) % mod;
-        res[k] = (a0[k] + t) % mod;
-        res[k + n / 2] = ((a0[k] - t) % mod + mod) % mod;
-        w = (w * 1ll * wn) % mod;
     }
     return res;
 }
 
-vector<int> ntt_inv(vector<int> a) {
-    a = ntt(a, true);
-    int n = a.size(), inv = binpow(n, mod - 2, mod);
-    for (int i = 0; i < n; ++i) {
-        a[i] = (a[i] * 1ll * inv) % mod;
+struct Z {
+    int x;
+    Z(int x = 0) : x(norm(x)) {}
+    int val() const {
+        return x;
     }
-    return a;
+    Z operator-() const {
+        return Z(norm(P - x));
+    }
+    Z inv() const {
+        assert(x != 0);
+        return power(*this, P - 2);
+    }
+    Z &operator*=(const Z &rhs) {
+        x = i64(x) * rhs.x % P;
+        return *this;
+    }
+    Z &operator+=(const Z &rhs) {
+        x = norm(x + rhs.x);
+        return *this;
+    }
+    Z &operator-=(const Z &rhs) {
+        x = norm(x - rhs.x);
+        return *this;
+    }
+    Z &operator/=(const Z &rhs) {
+        return *this *= rhs.inv();
+    }
+    friend Z operator*(const Z &lhs, const Z &rhs) {
+        Z res = lhs;
+        res *= rhs;
+        return res;
+    }
+    friend Z operator+(const Z &lhs, const Z &rhs) {
+        Z res = lhs;
+        res += rhs;
+        return res;
+    }
+    friend Z operator-(const Z &lhs, const Z &rhs) {
+        Z res = lhs;
+        res -= rhs;
+        return res;
+    }
+    friend Z operator/(const Z &lhs, const Z &rhs) {
+        Z res = lhs;
+        res /= rhs;
+        return res;
+    }
+};
+
+std::vector<int> rev;
+std::vector<Z> roots{0, 1};
+void dft(std::vector<Z> &a) {
+    int n = a.size();
+
+    if (int(rev.size()) != n) {
+        int k = __builtin_ctz(n) - 1;
+        rev.resize(n);
+        for (int i = 0; i < n; i++) {
+            rev[i] = rev[i >> 1] >> 1 | (i & 1) << k;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (rev[i] < i) {
+            std::swap(a[i], a[rev[i]]);
+        }
+    }
+    if (int(roots.size()) < n) {
+        int k = __builtin_ctz(roots.size());
+        roots.resize(n);
+        while ((1 << k) < n) {
+            Z e = power(Z(3), (P - 1) >> (k + 1));
+            for (int i = 1 << (k - 1); i < (1 << k); i++) {
+                roots[2 * i] = roots[i];
+                roots[2 * i + 1] = roots[i] * e;
+            }
+            k++;
+        }
+    }
+    for (int k = 1; k < n; k *= 2) {
+        for (int i = 0; i < n; i += 2 * k) {
+            for (int j = 0; j < k; j++) {
+                Z u = a[i + j];
+                Z v = a[i + j + k] * roots[k + j];
+                a[i + j] = u + v;
+                a[i + j + k] = u - v;
+            }
+        }
+    }
 }
 
-int main() {
-    int n;
-    cin >> n;
-    n = (1 << n);
-    vector<int> a(n), b(n);
-    cin >> a >> b;
-    n *= 2;
-    a.resize(n);
-    b.resize(n);
-    W1.resize(n + 1);
-    W2.resize(n + 1);
-    for (int i = 1; i <= n; i *= 2) {
-        W1[i] = binpow(root, (mod - 1) / i, mod);
-        W2[i] = binpow(W1[i], mod - 2, mod);
-    }
-    a = ntt(a);
-    b = ntt(b);
-    for (int i = 0; i < n; ++i) {
-        a[i] = (a[i] * 1ll * b[i]) % mod;
-    }
-    a = ntt_inv(a);
-    for (int i: a) {
-        cout << i << " ";
+void idft(std::vector<Z> &a) {
+    int n = a.size();
+    std::reverse(a.begin() + 1, a.end());
+    dft(a);
+    Z inv = (1 - P) / n;
+    for (int i = 0; i < n; i++) {
+        a[i] *= inv;
     }
 }
