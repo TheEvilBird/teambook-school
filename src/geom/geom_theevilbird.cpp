10c2a426f0ef1c1b07602f7adcdd40e459df04a9
TheEvilBird <62757686+TheEvilBird@users.noreply.github.com>
2022-11-22 01:11:52 +0300

    Vec -> Point

--- src/geom/geom_theevilbird.cpp
+++ src/geom/geom_theevilbird.cpp
@@ -14,11 +14,11 @@ const long double EPS = 1e-7;
  * degree = rad * 180 / PI
 */
 
-struct Vec {
+struct Point {
     ll x, y;
 
-    Vec() : x(0), y(0) {}
-    Vec(ll _x, ll _y) : x(_x), y(_y) {}
+    Point() : x(0), y(0) {}
+    Point(ll _x, ll _y) : x(_x), y(_y) {}
 
     ld len() const {
         return sqrtl(x * x + y * y);
@@ -28,7 +28,7 @@ struct Vec {
         return (x * x + y * y);
     }
 
-    Vec operator*(ll k) const {
+    Point operator*(ll k) const {
         return {x * k, y * k};
     }
 
@@ -37,44 +37,44 @@ struct Vec {
         y *= k;
     }
 
-    Vec operator+(const Vec other) const {
+    Point operator+(const Point other) const {
         return {x + other.x, y + other.y};
     }
 
-    Vec operator-(const Vec other) const {
+    Point operator-(const Point other) const {
         return {x - other.x, y - other.y};
     }
 
-    bool operator==(const Vec other) const {
+    bool operator==(const Point other) const {
         return x == other.x && y == other.y;
     }
 
-    bool operator!=(const Vec other) const {
+    bool operator!=(const Point other) const {
         return !(*this == other);
     }
 
-    ll operator*(const Vec other) const {// dot product
+    ll operator*(const Point other) const {// dot product
         return x * other.x + y * other.y;
     }
 
-    ll operator%(const Vec other) const {// cross product
+    ll operator%(const Point other) const {// cross product
         return x * other.y - y * other.x;
     }
 };
 
-istream &operator>>(istream &is, Vec &vec) {
+istream &operator>>(istream &is, Point &vec) {
     is >> vec.x >> vec.y;
     return is;
 }
 
-ostream &operator<<(ostream &os, const Vec &vec) {
+ostream &operator<<(ostream &os, const Point &vec) {
     os << vec.x << ' ' << vec.y;
     return os;
 }
 
-typedef vector<Vec> Polygon;
+typedef vector<Point> Polygon;
 
-bool cmp_vectors(Vec a, Vec b) {
+bool cmp_vectors(Point a, Point b) {
     if ((a.y < 0 || (a.y == 0 && a.x < 0)) &&
         (b.y > 0 || (b.y == 0 && b.x > 0))) {
         return true;
@@ -97,15 +97,15 @@ Polygon build_convex_hull(Polygon &a) {
     for (int i = 1; i < n; ++i) {
         if ((a[i].y < a[0].y) || (a[i].y == a[0].y && a[i].x < a[0].x)) swap(a[0], a[i]);
     }
-    sort(a.begin() + 1, a.end(), [&](Vec A, Vec B) {
-        Vec oa = A - a[0], ob = B - a[0];
+    sort(a.begin() + 1, a.end(), [&](Point A, Point B) {
+        Point oa = A - a[0], ob = B - a[0];
         if ((oa % ob) == 0) return oa.len_sq() < ob.len_sq();
         return (oa % ob) > 0;
     });
     Polygon hull = {a[0]};
     for (int i = 1; i < n; ++i) {
         while (sz(hull) >= 2) {
-            Vec ab = hull[sz(hull) - 1] - hull[sz(hull) - 2], bp = a[i] - hull[sz(hull) - 1];
+            Point ab = hull[sz(hull) - 1] - hull[sz(hull) - 2], bp = a[i] - hull[sz(hull) - 1];
             if ((ab % bp) <= 0) {
                 hull.pop_back();
             } else {
@@ -121,7 +121,7 @@ ll area_of_polygon(Polygon &poly) {
     ll res = 0;
     int n = sz(poly);
     for (int i = 2; i < n; ++i) {
-        Vec ab = poly[i - 1] - poly[0], ac = poly[i] - poly[0];
+        Point ab = poly[i - 1] - poly[0], ac = poly[i] - poly[0];
         res += (ab % ac);
     }
     // don't forget to divide the result by 2!
@@ -132,7 +132,7 @@ ld perimeter_of_polygon(Polygon &poly) {
     ld res = 0;
     int n = sz(poly);
     for (int i = 0; i < n; ++i) {
-        Vec v = poly[(i + 1 == n ? 0 : i + 1)] - poly[i];
+        Point v = poly[(i + 1 == n ? 0 : i + 1)] - poly[i];
         res += v.len();
     }
     return res;
@@ -144,7 +144,7 @@ ll diameter_of_polygon(Polygon &poly) {
     for (int i = 0; i < n; ++i) {
         int j = (i + 1) % n;
         while (true) {
-            Vec ac = poly[x] - poly[i], ab = poly[j] - poly[i], cd = poly[(x + 1) % n] - poly[x];
+            Point ac = poly[x] - poly[i], ab = poly[j] - poly[i], cd = poly[(x + 1) % n] - poly[x];
             ans = max(ans, ac.len_sq());
             if ((ab % cd) <= 0) {
                 break;
@@ -157,25 +157,25 @@ ll diameter_of_polygon(Polygon &poly) {
     return ans;
 }
 
-ld angle(const Vec &a, const Vec &b) {
+ld angle(const Point &a, const Point &b) {
     return fabsl(atan2(a % b, a * b) / PI * 180);
 }
 
-ld angle_rad(const Vec &a, const Vec &b) {
+ld angle_rad(const Point &a, const Point &b) {
     return (atan2(a % b, a * b));
 }
 
-ld angle_rad(const Vec &a) {
+ld angle_rad(const Point &a) {
     return atan2(a.y, a.x);
 }
 
-ld from_point_to_line(const Vec &p, const Vec &a, const Vec &b) {
-    Vec ba = a - b, ap = p - a;
+ld from_point_to_line(const Point &p, const Point &a, const Point &b) {
+    Point ba = a - b, ap = p - a;
     return fabs((ba % ap) / ba.len());
 }
 
-ld from_point_to_ray(const Vec &p, const Vec &a, const Vec &b) {
-    Vec ba = a - b, ap = p - a, ab = b - a;
+ld from_point_to_ray(const Point &p, const Point &a, const Point &b) {
+    Point ba = a - b, ap = p - a, ab = b - a;
     if ((ab * ap) < 0) {
         return ap.len();
     } else {
@@ -183,8 +183,8 @@ ld from_point_to_ray(const Vec &p, const Vec &a, const Vec &b) {
     }
 }
 
-ld from_point_to_segment(const Vec &p, const Vec &a, const Vec &b) {
-    Vec ab = b - a, ap = p - a, bp = p - b, ba = a - b;
+ld from_point_to_segment(const Point &p, const Point &a, const Point &b) {
+    Point ab = b - a, ap = p - a, bp = p - b, ba = a - b;
     if ((ab * ap) < 0) {
         return fabsl(ap.len());
     } else if ((ab * bp) > 0) {
@@ -194,29 +194,29 @@ ld from_point_to_segment(const Vec &p, const Vec &a, const Vec &b) {
     }
 }
 
-bool point_on_line(const Vec &p, const Vec &a, const Vec &b) {
-    Vec ab = b - a, ap = p - a;
+bool point_on_line(const Point &p, const Point &a, const Point &b) {
+    Point ab = b - a, ap = p - a;
     return (ab % ap) == 0;
 }
 
-bool point_on_ray(const Vec &p, const Vec &a, const Vec &b) {
-    Vec ab = b - a, ap = p - a;
+bool point_on_ray(const Point &p, const Point &a, const Point &b) {
+    Point ab = b - a, ap = p - a;
     return ((ab % ap) == 0 && (ab * ap) > 0) || (a == p);
 }
 
-bool point_on_segment(const Vec &p, const Vec &a, const Vec &b) {
-    Vec ap = p - a, bp = p - b, ab = b - a;
+bool point_on_segment(const Point &p, const Point &a, const Point &b) {
+    Point ap = p - a, bp = p - b, ab = b - a;
     return (ap * bp) <= 0 && (ap % ab) == 0;
 }
 
-bool point_in_angle(const Vec &p, const Vec &a, const Vec &o, const Vec &b) {
-    Vec oa = a - o, ob = b - o, op = p - o;
+bool point_in_angle(const Point &p, const Point &a, const Point &o, const Point &b) {
+    Point oa = a - o, ob = b - o, op = p - o;
     if ((oa % ob) < 0) swap(oa, ob);
     return ((oa % op) >= 0 && (ob % op) <= 0);
 }
 
-bool segment_intersection(const Vec &a, const Vec &b, const Vec &c, const Vec &d) {
-    Vec ab = b - a, cd = d - c, ac = c - a, ad = d - a, cb = b - c, ca = a - c;
+bool segment_intersection(const Point &a, const Point &b, const Point &c, const Point &d) {
+    Point ab = b - a, cd = d - c, ac = c - a, ad = d - a, cb = b - c, ca = a - c;
     if (get_sign((ab % ac)) * get_sign((ab % ad)) <= 0 && get_sign((cd % ca)) * get_sign((cd % cb)) <= 0) {
         ll x1 = max(min(a.x, b.x), min(c.x, d.x)), x2 = min(max(a.x, b.x), max(c.x, d.x));
         ll y1 = max(min(a.y, b.y), min(c.y, d.y)), y2 = min(max(a.y, b.y), max(c.y, d.y));
@@ -225,16 +225,16 @@ bool segment_intersection(const Vec &a, const Vec &b, const Vec &c, const Vec &d
     return false;
 }
 
-bool rays_intersection(const Vec &a, const Vec &b, const Vec &c, const Vec &d) {
+bool rays_intersection(const Point &a, const Point &b, const Point &c, const Point &d) {
     // TODO
 }
 
-bool lines_intersection(const Vec &a, const Vec &b, const Vec &c, const Vec &d) {
-    Vec ab = b - a, cd = d - c;
+bool lines_intersection(const Point &a, const Point &b, const Point &c, const Point &d) {
+    Point ab = b - a, cd = d - c;
     return ((ab % cd) != 0);
 }
 
-int point_in_polygon(const Vec &p, const Polygon &poly) {
+int point_in_polygon(const Point &p, const Polygon &poly) {
     // 0 - outside, 1 - inside, 2 - border;
     int n = sz(poly);
     if (point_in_angle(p, poly[n - 1], poly[0], poly[1])) {
@@ -265,7 +265,7 @@ int point_in_polygon(const Vec &p, const Polygon &poly) {
     }
 }
 
-int point_in_nonconvex_polygon(const Vec &p, const Polygon &poly) {
+int point_in_nonconvex_polygon(const Point &p, const Polygon &poly) {
     // 0 - outside, 1 - inside, 2 - border;
     int n = sz(poly);
     for (int i = 0; i < n; ++i) {
@@ -275,7 +275,7 @@ int point_in_nonconvex_polygon(const Vec &p, const Polygon &poly) {
     }
     ld s = 0.0;
     for (int i = 0; i < n; ++i) {
-        Vec pa = poly[i] - p, pb = poly[(i + 1 == n ? 0 : i + 1)] - p;
+        Point pa = poly[i] - p, pb = poly[(i + 1 == n ? 0 : i + 1)] - p;
         s += angle_rad(pa, pb);
     }
     if (s >= PI || s <= -PI) {
@@ -289,7 +289,7 @@ Polygon minkowski_sum(Polygon &a, Polygon &b) {
 //    a[0], b[0]: y - max, y1 = y2 => x - max. Against clockwise
     int n = sz(a), m = sz(b);
     assert(n >= 3 && m >= 3);
-    Vec high_a = a[0], high_b = b[0];
+    Point high_a = a[0], high_b = b[0];
     Polygon va(n), vb(m);
     for (int i = 0; i < n; ++i) {
         va[i] = a[(i + 1) % n] - a[i];
@@ -301,7 +301,7 @@ Polygon minkowski_sum(Polygon &a, Polygon &b) {
 //    sort(all(vb), cmp_vectors);
     Polygon vc(sz(va) + sz(vb));
     merge(all(va), all(vb), vc.begin(), cmp_vectors);
-    Vec high_c(high_a.x + high_b.x, high_a.y + high_b.y);
+    Point high_c(high_a.x + high_b.x, high_a.y + high_b.y);
     Polygon c(sz(vc) + 1);
     c[0] = high_c;
     for (int i = 0; i < sz(c) - 1; ++i) {
@@ -324,7 +324,7 @@ ld from_polygon_to_polygon(Polygon a, Polygon b) {
     rotate(b.begin(), b.begin() + pos, b.end());
     Polygon c = minkowski_sum(a, b);
     int n = sz(c);
-    Vec p(0, 0);
+    Point p(0, 0);
     ld ans = 1e20;
     for (int i = 0; i < n - 1; ++i) {
         ans = min(ans, from_point_to_segment(p, c[i], c[i + 1]));
